769e8b880aac2e43e83f677197970bd37d2692fc
Abdul Muqeet <39890909+abdulmuqeet1@users.noreply.github.com>
2020-10-13 17:38:58 +0500

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+
+        
+            <h2>
+                Expense Tracker
+            </h2>
+            
+        </header>
+    )
+}
